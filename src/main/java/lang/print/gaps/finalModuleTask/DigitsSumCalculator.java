87cde6d9aeaf881f1ee8a5bd39c270802224453e
yoqub-dev <yoqub.epam@gmail.com>
2023-10-30 00:50:50 +0500

    task6

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+
+        int a = number % 10;
+        int b = number % 100/10;
+        int c = number % 1000/100;
+        int d = number % 10000/100/10;
+        System.out.println(a+b+c+d);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int first  = number % 10;
+        int second  = number % 100/10;
+        int tree  = number % 1000/100;
+        System.out.println(first+""+second+""+tree);
     }
 }
